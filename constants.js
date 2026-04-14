f7d773f2be9e40c62ca93fbf3da19e2ce175509b
donsweeney4 <donsweeney4@gmail.com>
2026-04-13 21:14:48 -0700

    new constants

--- constants.js
+++ constants.js
@@ -9,7 +9,7 @@ export const VERSION = "4.2.2";
 // They should be incremented for every release except local development builds. The current values can be found in the app stores or in the respective configuration files for iOS and Android.
 
 export const IOS_BUILD_NUMBER = "5";  // Increment this for every TestFlight or store (preview or production), get current value from appstoreconnect.apple.com
-export const ANDROID_VERSION_CODE = "4"; // Increment this for every sideload (preview .APK) or Play Store release (production .AAB), get current value from play.google.com/console or expo.com or from the AndroidManifest.xml file in the android/app/src/main directory
+export const ANDROID_VERSION_CODE = "5"; // Increment this for every sideload (preview .APK) or Play Store release (production .AAB), get current value from play.google.com/console or expo.com or from the AndroidManifest.xml file in the android/app/src/main directory
 
 
 export const LOCATION_TASK_NAME = "background-location-task";
